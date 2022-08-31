d0dc64bbaf826bc0e0ec8d5cc0ca9aef46b42364
Khiem Truong <khiemtruongxuan@gmail.com>
2022-08-31 12:25:56 +1000

    updated web and added IOS development for week 6

--- week4/personal-portfolio/src/components/Skills.js
+++ week4/personal-portfolio/src/components/Skills.js
@@ -27,7 +27,7 @@ export const Skills = () => {
 
 
 
-  const Progress = ({done}) => {
+  const Progress = ({done, name}) => {
     const [style, setStyle] = React.useState({});
     
     setTimeout(() => {
@@ -42,7 +42,7 @@ export const Skills = () => {
     return (
       <div className="progress">
         <div className="progress-done" style={style}>
-          {done}%
+        {name} 
         </div>
       </div>
     )
@@ -61,75 +61,50 @@ export const Skills = () => {
                             <div className="item">
                               <h3 className = "skillMainTitle">Mobile Development</h3>
                               <Row className='skillSet'>
-                                <Col lg={12} md={2} xl={10}>
-                                <Progress done="90"/>
-                                <Progress done="65"/>
-                                <Progress done="70"/>
-                                <Progress done="75"/>
+                                <Col lg={12} md={12} xl={12}>
+                                <Progress done="90" name = "Flutter"/>
+                                <Progress done="65" name = "React Native"/>
+                                <Progress done="70" name = "Swift"/>
+                                <Progress done="75" name = "Kotlin"/>
                                 </Col>
-                                <Col md={2} xl={2}>
-                                <h6 className='skillTitle'>Flutter</h6>
-                                <h6 className='skillTitle'>React Native</h6>
-                                <h6 className='skillTitle'>Swift</h6>
-                                <h6 className='skillTitle'>Kotlin</h6>
-                                </Col>
+                    
                               </Row>
                             </div>
                             <div className="item">
                               <h3 className = "skillMainTitle">Frontend Development</h3>
                               <Row className='skillSet'>
-                                <Col lg={12} md={2} xl={10}>
-                                <Progress done="70"/>
-                                <Progress done="80"/>
-                                <Progress done="80"/>
-                                </Col>
-                                <Col md={2} xl={2}>
-                                <h6 className='skillTitle'>React JS</h6>
-                                <h6 className='skillTitle'>Svelte</h6>
-                                <h6 className='skillTitle'>HTML, CSS, Bootstrap</h6>
+                                <Col lg={12} md={12} xl={12}>
+                                <Progress done="70" name={"React JS"}/>
+                                <Progress done="80" name={"Svelte"}/>
+                                <Progress done="80" name={"HTML, CSS, Bootstrap"}/>
                                 </Col>
                               </Row>
                             </div>
                             <div className="item">
                               <h3 className = "skillMainTitle">Backend Development</h3>
                               <Row className='skillSet'>
-                                <Col lg={12} md={2} xl={10}>
-                                <Progress done="75"/>
-                      
-                                </Col>
-                                <Col md={2} xl={2}>
-                                <h6 className='skillTitle'>Node JS</h6>
-                               
+                                <Col lg={12} md={12} xl={12}>
+                                <Progress done="75" name={"Node JS"}/>
                                 </Col>
                               </Row>
                             </div>
                             <div className="item">
                               <h3 className = "skillMainTitle">Services</h3>
                               <Row className='skillSet'>
-                                <Col lg={12} md={2} xl={10}>
-                                <Progress done="85"/>
-                                <Progress done="70"/>
-                                <Progress done="70"/>
-                                </Col>
-                                <Col md={2} xl={2}>
-                                <h6 className='skillTitle'>Firebase</h6>
-                                <h6 className='skillTitle'>AWS</h6>
-                                <h6 className='skillTitle'>Microsoft Azure</h6>
+                                <Col lg={12} md={12} xl={12}>
+                                <Progress done="85" name={"Firebase"}/>
+                                <Progress done="70" name={"AWS"}/>
+                                <Progress done="70" name={"Microsoft Azure"}/>
                                 </Col>
                               </Row>
                             </div>
                             <div className="item">
                               <h3 className = "skillMainTitle">Other Programing Languages</h3>
                               <Row className='skillSet'>
-                                <Col lg={12} md={2} xl={10}>
-                                <Progress done="80"/>
-                                <Progress done="70"/>
-                                <Progress done="70"/>
-                                </Col>
-                                <Col md={2} xl={2}>
-                                <h6 className='skillTitle'>Python</h6>
-                                <h6 className='skillTitle'>Java</h6>
-                                <h6 className='skillTitle'>Java Script</h6>
+                                <Col lg={12} md={12} xl={12}>
+                                <Progress done="80" name={"Python"}/>
+                                <Progress done="70" name={"Java"}/>
+                                <Progress done="70" name={"JavaScript"}/>
                                 </Col>
                               </Row>
                             </div>
---
+++ week6/IOS Health Reporter/IOS Health Reporter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS Health Reporter
+//
+//  Created by Truong Xuan Khiem on 29/8/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
