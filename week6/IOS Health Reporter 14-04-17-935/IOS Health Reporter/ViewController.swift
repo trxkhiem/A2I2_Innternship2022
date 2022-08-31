ddf918cd8a626d533a727da4d9958f5d041bf4fe
Khiem Truong <khiemtruongxuan@gmail.com>
2022-08-31 15:00:13 +1000

    Update IOS development

--- week6/IOS Health Reporter 14-04-17-935/IOS Health Reporter/ViewController.swift
+++ week6/IOS Health Reporter 14-04-17-935/IOS Health Reporter/ViewController.swift
@@ -12,23 +12,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var roundButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         roundButton.layer.borderWidth = 5
         roundButton.layer.borderColor = UIColor.orange.cgColor
         roundButton.layer.cornerRadius = roundButton.frame.width/2
         roundButton.layer.masksToBounds = true
         
         roundButton.addTarget(self, action: #selector(getStart), for: .touchUpInside)
-        // Do any additional setup after loading the view.
     }
-    @objc private func getStart(){
-        guard let url = URL(string: "https://main.d2a8gmdwh094j5.amplifyapp.com/") else {
-            return
-        }
-        let vc = WebViewController(url: url, title: "IOS Health Reporter")
-        
-        let navVC = UINavigationController(rootViewController: vc)
-        present(navVC, animated: true)
-    } 
 
+    @objc private func getStart(){
+        
+    }
+    
 }
 
---
+++ week6/IOS Health Reporter 14-04-17-935/IOS Health Reporter/Web ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Web ViewController.swift
+//  IOS Health Reporter
+//
+//  Created by Truong Xuan Khiem on 31/8/2022.
+//
+
+import UIKit
+
+class Web_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
