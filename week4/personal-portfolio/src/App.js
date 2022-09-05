f7c5f498f30ff7b949f883bb6558c98f59124105
Khiem Truong <khiemtruongxuan@gmail.com>
2022-09-05 12:47:17 +1000

    setup week 7

--- week4/personal-portfolio/src/App.js
+++ week4/personal-portfolio/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
@@ -9,6 +11,19 @@ import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    Axios.get("https://ke7gmpy835.execute-api.ap-southeast-2.amazonaws.com/myFirstFunction").then(
+      (response) => {
+        console.log("test api")
+        console.log(response)
+      }
+    );
+  });
+
   return (
     <div className="App">
       <NavBar />
